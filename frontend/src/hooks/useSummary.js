61172c9840a56108b8ba07d94031fcd4d60fb5cd
mr_vau <mdalif1434@gmail.com>
2026-07-09 09:15:03 +0600

    remove unnecessary files

--- frontend/src/hooks/useSummary.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import { SummaryContext } from "../context/summaryContext"
-
-export const useSummary = () => {
-  const context = useContext(SummaryContext)
-  if(!context) throw new Error("useSummary must be used within a SummaryProvider")
-  
-  return context
-}
